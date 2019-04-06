daade5f1a7fe5b8a9aae1f6f36a012e72085e188
Nadav <nbassell@gmail.com>
2019-04-06 19:17:07 -0400

    Minor heart changes

--- src/js/heart.js
+++ src/js/heart.js
@@ -8,7 +8,6 @@ export default class Heart {
   }
   
   startHeart() {
-    debugger
     var x = 0, y = 0;
     var heartShape = new THREE.Shape();
 
@@ -29,9 +28,11 @@ export default class Heart {
     box.getCenter(mesh.position); // this re-sets the mesh position
     mesh.position.multiplyScalar( - 1 );
     this.pivot = new THREE.Group();
-    debugger
     this.scene.add(this.pivot);
     this.pivot.add(mesh);
+
+    this.pivot.position.set(-100,100,-200)
+    
     return this.pivot;
   }
 
