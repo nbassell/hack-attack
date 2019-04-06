f7808d85bf06125e38178b4932d7e7f03d278f4b
agronv <avelaj96@gmail.com>
2019-04-06 19:30:41 -0400

    Render enemy ships

--- src/js/enemies.js
+++ src/js/enemies.js
@@ -3,14 +3,17 @@ import Enemy from './enemy'
 export default class Enemies {
     constructor(scene) {
         this.enemies = [];
+        this.positions = [[-10,-10], [-10,0], [-10,10], [0, -10], [0,0], [0,10], [10,-10], [10,0], [10,10]]
         this.scene = scene;
         this.spawnEnemies();
     }
 
     spawnEnemies() {
         setInterval(() => {
-            let enemy = new Enemy();
-            this.scene.add(enemy.enemy);
+            let random = Math.floor(Math.random() * 9);
+            let position = this.positions[random]
+            let enemy = new Enemy(position, this.scene);
+            // this.scene.add(enemy.enemy);
             this.enemies.push(enemy);
         }, 1000);
     }
--- src/js/enemy.js
+++ src/js/enemy.js
@@ -1,17 +1,25 @@
 import * as THREE from 'three';
 
 export default class Enemey {
-  constructor() {
+  constructor(position, scene) {
+    this.position = position;
+    this.scene = scene;
     this.enemy = this.startEnemy();
   }
   
   startEnemy() {
 
+    var loader = new THREE.ObjectLoader();
+    loader.load( 'models/enemy.json', (object) => {
+      this.scene.add(object);
+    });
 
-    var geometry = new THREE.BoxBufferGeometry(3, 1, 1);
-    var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    var mesh = new THREE.Mesh( geometry, material ) ;
-    return mesh;
+    // var geometry = new THREE.BoxBufferGeometry(10, 1, 1);
+    // var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
+    // var object = new THREE.Mesh( geometry, material );
+    object.position.x = this.position[0];
+    object.position.y = this.position[1];
+    return object;
   }
 
   updatePos() {
