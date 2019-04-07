2f3549951403849832d8b6e26c0932a2357755a5
agronv <avelaj96@gmail.com>
2019-04-07 02:59:37 -0400

    Pass position info into enemies

--- src/js/enemy.js
+++ src/js/enemy.js
@@ -2,12 +2,14 @@
 import 'three/examples/js/loaders/GLTFLoader';
 
 export default class Enemy {
-  constructor(position, scene, speed) {
+  constructor(position, scene, speed, playerPos) {
     this.position = position;
     this.scene = scene;
     this.speed = speed;
+    this.playerPos = playerPos;
     this.changeX = -1*(this.position[0]/this.speed);
-    this.changeY = -1*(this.position[1]/this.speed)
+    this.changeY = -1*((this.position[1]-this.playerPos[1])/this.speed);
+    this.changeZ = (this.position[2]-this.playerPos[2]) / this.speed;
     
     this.setEnemy = this.setEnemy.bind(this);
     this.startEnemy();
@@ -19,22 +21,13 @@ export default class Enemy {
     return loader.load('src/models/player/scene.gltf', this.setEnemy, undefined, function (error) {
       console.error(error);
     });
-
-    // var geometry = new THREE.BoxBufferGeometry(1, 1, 0.5);
-    // var material = new THREE.MeshBasicMaterial( { color: 0xff0000 } );
-    // var object = new THREE.Mesh( geometry, material );
-    // object.position.x = this.position[0];
-    // object.position.y = this.position[1];
-    // object.position.z = -54;
-    // this.scene.add(object);
-    // return object;
   }
 
   setEnemy(enemy) {
     enemy.scene.position.x
     enemy.scene.position.x = this.position[0];
     enemy.scene.position.y = this.position[1];
-    enemy.scene.position.z = -54;
+    enemy.scene.position.z = this.position[2];
     this.scene.add(enemy.scene);
     this.enemy = enemy;
   }
